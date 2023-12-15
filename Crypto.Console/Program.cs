3fb49571a8f2c7c09d1a357fdeb4e85ec4afd974
Jim Campbell <jimcampbell355@gmail.com>
2023-12-14 20:53:43 -0500

    Updated the program.

--- Crypto.Console/Program.cs
+++ Crypto.Console/Program.cs
@@ -1,4 +1,11 @@
-﻿string instructions = """
+﻿using Crypto.Cryptography;
+using static System.Net.Mime.MediaTypeNames;
+
+string pathInput = Path.Combine(Environment.CurrentDirectory, "input.txt");
+string pathOutput = Path.Combine(Environment.CurrentDirectory, "output.txt");
+string emptyTextMessage = "Text is empty, did you forget to fill out the input.txt file?";  
+
+string instructions = """
     Enter your encrypted/decrypted text, into the input.txt file.
     Enter your 8 byte encryption key when prompted.
     Tell the application whether you want to encrypt or decrypt the text.
@@ -11,16 +18,23 @@ while (!invalidInput)
     try
     {
         WriteLine("Enter your 8 byte encryption key: ");
-        string key = ReadLine();
+        string? key = ReadLine();
+        if (key == null)
+        {
+            invalidInput = true;
+            WriteLine("Invalid input, did not enter a key.");
+        }
         WriteLine("Do you want to [E]ncrypt or [D]ecrypt? (E/D)");
         ConsoleKey ck = Console.ReadKey().Key;
         switch (ck)
         {
             case ConsoleKey.E:
                 Encrypt(key);
+                WriteLine("Text encrypted successfully!");
                 break;
             case ConsoleKey.D:
                 Decrypt(key);
+                WriteLine("Text decrypted successfully!");
                 break;
             default:
                 WriteLine("Invalid input");
@@ -38,6 +52,35 @@ while (!invalidInput)
     }
 }
 
+void Decrypt(string key)
+{
+    string text = ReadText();
+    if (text == null || text.Length == 0) throw new ArgumentException(emptyTextMessage);
+    string decryptedText = Cryptographer.Decrypt(text, key);
+    WriteText(decryptedText);
 
-string path = Path.Combine(Environment.CurrentDirectory, "input.txt");
-using (StreamReader sr = new)
+}
+
+void Encrypt(string? key)
+{
+    string text = ReadText();
+    if (text == null || text.Length == 0) throw new ArgumentException(emptyTextMessage);
+    string encryptedText = Cryptographer.Encrypt(text, key);
+    WriteText(encryptedText);
+}
+
+string ReadText()
+{
+    using (StreamReader sr = new StreamReader(pathInput))
+    {
+        string? text = sr.ReadToEnd();
+        return text;
+    }
+}
+void WriteText(string decryptedText)
+{
+    using (StreamWriter sw = new StreamWriter(pathOutput))
+    {
+        sw.Write(decryptedText);
+    }
+}
